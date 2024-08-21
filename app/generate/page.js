e63e90c3e13f87e219eed3f80709675f94cc4c48
nk0311 <navaitk031@gmail.com>
2024-08-21 17:14:13 -0400

    added react component to generate flashcards

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,6 +1,66 @@
 'use client'
+
+import {useUser} from '@clerk/nextjs'
+import {useRouter} from 'next/navigation'
+
 export default function Generate() {
     const {isLoaded, isSignedIn, user} = useUser()
-    const [flashcard]
+    const [flashcards, setFlashcards] = useState([])
+    const [flipped, setFlipped] = useState([])
+    const [text, setText] = useState('')
+    const [name, setName] = useState('')
+    const [open, setOpen] = useState(false)
+    const router = useRouter()
+
+    const handleSubmit=async ()=> {
+
+    fetch('api/generate', {
+        method: 'POST',
+        body: text,
+    })
+    .then((res)=> res.json())
+    .then((data)> setFlashcards(data))
+    }
+
+    const handleCardClick = (id) =>{
+        setFlipped((prev) => ({
+            ...prev,
+            [id]: !prev[id]
+        }))
+    }
+
+    const handleOpen = () =>{
+        setOpenI(true)
+    }
+    const handleClose = () =>{
+        setOpenI(false)
+    }
+
+    const saveFlashcards = async () =>{
+        if (!name){
+            alert('Please enter a name')
+            return
+        }
+        
+        const batch = writeBatch(db)
+        const userDocRef = doc(collectioN(db, 'users'), user.id)
+        const docSnap = await getDoc(userDocRef)
+
+        if (docSnap.exists()){
+            const collections = docSnap.data().flashcards || []
+            if (collections.find((f)=> f.name === name)) {
+                alert("Flashcard collection with the same name already exists.")
+                return
+            }
+            else{
+                collections.push({name})
+                batch.set(userDocRed, {flashcards: collections}, {merge})
+            }
+        }
+
+    }
+
+
+
 
 }
